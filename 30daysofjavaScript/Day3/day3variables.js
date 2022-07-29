8b5461c09aa34205ce3fca8f8b9bb845530a882d
Schluterman/laptop <60680674+Schluterman@users.noreply.github.com>
2022-07-28 20:44:44 -0500

    progress q1-5 Day3

---
+++ 30daysofjavaScript/Day3/day3variables.js
@@ -0,0 +1,48 @@
+//Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it and use the typeof operator to check different data types.
+let firstName = typeof'Preston'
+let lastName = typeof'Schluterman'
+let country = typeof'USA'
+let age = typeof (23); 
+let isMarried = false; 
+const now = new Date()
+const year = now.getFullYear()
+console.log(typeof('10')); //come back to later 
+console.log(typeof(parseInt('9.8')));//come back to later
+
+//Boolean True and falsely 
+console.log(3>6); //false 
+console.log(4<2); //false
+console.log(2===1);//false 
+console.log(2==2); //true
+console.log(9>3); //true
+console.log(2<4); //true
+
+
+console.log(4 > 3);//true
+console.log(4 >= 3);//true
+console.log(4 < 3);//false
+console.log(4 <= 3);//false
+console.log(4 == 4);//true
+console.log(4 === 4);//true
+console.log(4 != 4);//false
+console.log(4 !== 4);//false
+console.log(4 != '4');//false
+console.log(4 == '4');//true
+console.log(4 === '4');//false
+let python = 'Python';
+let jargon ='Jargon'
+console.log(python.length < jargon.length);
+
+console.log(4 > 3 && 10 < 12)//t Correct
+console.log(4 > 3 && 10 > 12)//f Correct
+console.log(4 > 3 || 10 < 12)//f Wrong
+console.log(4 > 3 || 10 > 12)//f Wrong
+console.log(!(4 > 3))//f Correct
+console.log(!(4 < 3))//t Correct
+console.log(!(false))//t Correct
+console.log(!(4 > 3 && 10 < 12))//f Correct
+console.log(!(4 > 3 && 10 > 12))//t Correct
+console.log(!(4 === '4'))//t Correct
+console.log (!'on' ) //There is no 'on' in both dragon and python No Clue 
+//Date Object examples 
+
